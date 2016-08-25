6c4f8f86da586137b9a6b17e0a476a44337805dd
Regan Sarwas <regan_sarwas@nps.gov>
2016-08-25 07:20:54 -0800

    Create a new cmdline project

---
+++ tm/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NPS.AKRO.ThemeManager.ArcGIS;
+using NPS.AKRO.ThemeManager.Extensions;
+using NPS.AKRO.ThemeManager.Model;
+
+namespace tm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting ESRI License");
+            EsriLicenseManager.Start(false);
+            if (!EsriLicenseManager.Running)
+            {
+                Console.WriteLine($"Could not initialize an ArcGIS license. {EsriLicenseManager.Message}");
+                return;
+            }
+            var path = args[0];
+            Console.WriteLine($"Loading {path}");
+            var themeList = Load(path);
+            themeList.SuspendUpdates();
+            themeList.Build();
+            Reload(themeList);
+            themeList.SaveAs(path);
+        }
+
+        static TmNode Load(String path)
+        {
+            return new TmNode(TmNodeType.ThemeList,
+                Path.GetFileNameWithoutExtension(path),
+                null, new ThemeData(path), null, null, null);
+        }
+
+        static void Reload(TmNode root)
+        {
+            if (root == null)
+            {
+                Console.WriteLine("No node provided for reload");
+                return;
+            }
+
+            List<TmNode> nodes = root.Recurse(x => x.Children)
+                                        .Where(n => n.IsTheme)
+                                        .ToList();
+
+            Console.WriteLine($"Updating {nodes.Count} Themes");
+            foreach (var node in nodes)
+            {
+                try
+                {
+                    node.ReloadTheme();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\nError reloading node {node.Name}: {ex.Message}");
+                }
+                try
+                {
+                    node.SyncWithMetadata();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\nError syncing metadata for node {node.Name}: {ex.Message}");
+                }
+                Console.Write(".");
+            }
+            Console.WriteLine("\nDone.");
+        }
+
+    }
+}
---
+++ tm/Shims.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace NPS.AKRO.ThemeManager.Properties
+{
+    class Settings
+    {
+        public static Default Default = new Default()
+        {
+            AgeInDays = 30,
+            KeepMetaDataInMemory = false,
+            CheckForArcViewBeforeArcInfo = false,
+        };
+    }
+
+    class Default
+    {
+        public int AgeInDays { get; set; }
+        public bool KeepMetaDataInMemory { get; set; }
+        public bool CheckForArcViewBeforeArcInfo { get; set; }
+    }
+
+}
+
+
+namespace NPS.AKRO.ThemeManager.UI.Forms
+{
+    class LoadingForm
+    {
+        public string Message { get; set; }
+        public string Path { get; set; }
+        public string Command { get; set; }
+
+        public string LoadMetadataWithCatalog { get; set; }
+        public string LoadLicense { get; set; }
+
+        public void ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+namespace NPS.AKRO.ThemeManager.Model
+{
+    internal class StyleSheet
+    {
+        public string XformText(string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
