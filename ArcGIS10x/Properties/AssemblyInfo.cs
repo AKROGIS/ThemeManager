5f302f252c1af5f38a90ade4a680979322153388
Regan Sarwas <regan_sarwas@nps.gov>
2021-06-10 12:05:26 -0800

    Remove CLSCompliance
    1) ArcObjects classes are not CLSCompliant
    2) Not relevant for apps
    3) My libraries are not intended for general use

--- ArcGIS10x/Properties/AssemblyInfo.cs
+++ ArcGIS10x/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -13,8 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © National Park Service 2015-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
