247f3a883b14ad74971c31f43b9615c29d969eb4
Regan Sarwas <regan_sarwas@nps.gov>
2021-06-10 11:53:18 -0800

    Implement and test a no GIS solution.

---
+++ NoGis/GisInterface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+// The following issues result from supporting an expected public API
+#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+namespace NPS.AKRO.ThemeManager.ArcGIS
+{
+    public class GisInterface
+    {
+        public static XsltArgumentList EsriProcessingArguments()
+        {
+            return new XsltArgumentList();
+        }
+
+        public static string CleanEsriMetadataHtml(string html)
+        {
+            return html;
+        }
+
+        public static async Task<IGisLayer> ParseItemAtPathAsGisLayerAsync(string path)
+        {
+            return new GisLayer();
+        }
+
+        public static async Task<string> GetMetadataAsXmlAsync(string path)
+        {
+            throw new ApplicationException("A GIS application must be installed to display this metadata.");
+        }
+
+        public static bool IsInitialized => true;
+
+        public static string Status => "No GIS Support avaialble.";
+
+        public static async Task<bool> InitializeAsync()
+        {
+            return true;
+        }
+
+        public static string ProgressorMessage => null;
+
+    }
+}
+
---
+++ NoGis/PreviewPage.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// The following issues result from supporting an expected public API
+#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+namespace NPS.AKRO.ThemeManager.ArcGIS
+{
+    public class PreviewPage
+    {
+        private readonly Control _defaultControl;
+
+        public PreviewPage(Control parent, Form form)
+        {
+            _defaultControl = parent.Controls[0];
+        }
+
+        public void ShowText(string text)
+        {
+            _defaultControl.Text = text;
+        }
+
+        public async Task ShowMapAsync(string path)
+        {
+            var msg = "Map preview is only available with ArcGIS 10.x installed.";
+            msg += $"\nUnable to display {path}.";
+            _defaultControl.Text = msg;
+        }
+
+    }
+}
