29602f7255fbe5ba622433d6850e71d44cd51e80
Regan Sarwas <regan_sarwas@nps.gov>
2021-06-18 16:30:47 -0800

    Add Pro build for Theme Manager

---
+++ ThemeManagerPro/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Theme ManagerPro")]
+[assembly: AssemblyDescription("A tool to help organize GIS data")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("National Park Service, Alaska Region GIS Team")]
+[assembly: AssemblyProduct("ThemeManagerPro")]
+[assembly: AssemblyCopyright("Copyright © National Park Service 2010-2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d541c7c5-7b66-431b-9f63-e5549f6f7ac6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.3.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
