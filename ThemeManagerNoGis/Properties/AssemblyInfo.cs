3c99eaa519b007c7d6f88fa38624812055ba6356
Regan Sarwas <regan_sarwas@nps.gov>
2021-06-18 16:09:11 -0800

    Add A No GIS version of Theme Manager

---
+++ ThemeManagerNoGis/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Theme ManagerNoGis")]
+[assembly: AssemblyDescription("A tool to help organize GIS data")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("National Park Service, Alaska Region GIS Team")]
+[assembly: AssemblyProduct("ThemeManagerNoGis")]
+[assembly: AssemblyCopyright("Copyright © National Park Service 2010-2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05f6626b-55c1-405a-8fbe-4dbd715e4035")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.3.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
